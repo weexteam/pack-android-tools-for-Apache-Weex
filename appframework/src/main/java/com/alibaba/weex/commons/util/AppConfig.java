22cee0ab59eadbdca2da26ea82227f4485bcd369
卜道 <705040424@qq.com>
2016-12-16 19:31:13 +0800

    add plugin module

--- appframework/src/main/java/com/alibaba/weex/commons/util/AppConfig.java
+++ appframework/src/main/java/com/alibaba/weex/commons/util/AppConfig.java
@@ -16,104 +16,47 @@ import java.util.HashMap;
 public class AppConfig {
   private static final String TAG = "AppConfig";
   private static final boolean DEBUG = false;
-  private static String mLocalUrl = "file://assets/index.js";
-  private static String mLaunchUrl = "http://127.0.0.1:8080/dist/index.js";
-  private static Boolean mIsLaunchLocally = false;
-  private static HashMap<String, String> sComponents = new HashMap<>();
-  private static HashMap<String, String> sModules = new HashMap<>();
+  private static HashMap<String, PluginEntry> sComponents = new HashMap<>();
+  private static HashMap<String, PluginEntry> sModules = new HashMap<>();
+  private static CordovaPreferences sPreferences = new CordovaPreferences();
 
   public static void init(Context context) {
-    loadAppSettings(context);
+    loadAppSetting(context);
   }
 
   public static String getLaunchUrl() {
-    return mLaunchUrl;
+    return sPreferences.getString("launch_url", "http://127.0.0.1:8080/dist/index.js");
   }
 
   public static String getLocalUrl() {
-    return mLocalUrl;
+    return sPreferences.getString("local_url", "file://assets/index.js");
   }
 
   public static Boolean isLaunchLocally() {
-    return mIsLaunchLocally;
+    return sPreferences.getBoolean("launch_locally", false);
   }
 
-  private static void loadAppSettings(Context context) {
-    int id = context.getResources().getIdentifier("app_config", "xml", context.getClass().getPackage().getName());
-    if (id == 0) {
-      // If we couldn't find config.xml there, we'll look in the namespace from AndroidManifest.xml
-      id = context.getResources().getIdentifier("app_config", "xml", context.getPackageName());
-      if (id == 0) {
-        Log.e(TAG, "res/xml/config.xml is missing!");
-        return;
-      }
-    }
-    XmlResourceParser parser = context
-        .getResources()
-        .getXml(id);
-    try {
-      XmlUtils.beginDocument(parser, "app_config");
-      while (true) {
-        XmlUtils.nextElement(parser);
-        String tag = parser.getName();
-        if (tag == null) {
-          break;
-        }
-        String name = parser.getAttributeName(0);
-        String value = parser.getAttributeValue(0);
-        String text = null;
-        if (parser.next() == XmlPullParser.TEXT) {
-          text = parser.getText();
-        }
-        if (DEBUG) {
-          Log.v(TAG, "tag: " + tag + " value: " + value);
-        }
-        if ("name".equalsIgnoreCase(name)) {
-          if ("bool".equals(tag)) {
-            if ("launch_locally".equalsIgnoreCase(value)) {
-              mIsLaunchLocally = Boolean.parseBoolean(text);
-            }
-          } else if ("int".equals(tag)) {
-//            if ("tag B".equalsIgnoreCase(value)) {
-//               mMaxMessageSize = Integer.parseInt(text);
-//            }
-          } else if ("string".equals(tag)) {
-            if ("launch_url".equalsIgnoreCase(value)) {
-              mLaunchUrl = text;
-            } else if ("local_url".equalsIgnoreCase(value)) {
-              mLocalUrl = text;
-            }
-          } else if ("component".equals(tag)) {
-            sComponents.put(value, text);
-          } else if ("module".equals(tag)) {
-            sModules.put(value, text);
-          }
-        }
-      }
-    } catch (XmlPullParserException e) {
-      Log.e(TAG, "loadAppSettings caught ", e);
-    } catch (NumberFormatException e) {
-      Log.e(TAG, "loadAppSettings caught ", e);
-    } catch (IOException e) {
-      Log.e(TAG, "loadAppSettings caught ", e);
-    } finally {
-      parser.close();
-    }
+  private static void loadAppSetting(Context context) {
+    ConfigXmlParser parser = new ConfigXmlParser();
+    parser.parse(context);
+    sComponents = parser.getPluginComponents();
+    sModules = parser.getPluginModules();
+    sPreferences = parser.getPreferences();
   }
 
-  public static HashMap<String, String> getComponents() {
+  public static HashMap<String, PluginEntry> getComponents() {
     return sComponents;
   }
 
-  public static void setComponents(HashMap<String, String> components) {
+  public static void setComponents(HashMap<String, PluginEntry> components) {
     AppConfig.sComponents = components;
   }
 
-  public static HashMap<String, String> getModules() {
+  public static HashMap<String, PluginEntry> getModules() {
     return sModules;
   }
 
-  public static void setModules(HashMap<String, String> modules) {
+  public static void setModules(HashMap<String, PluginEntry> modules) {
     AppConfig.sModules = modules;
   }
 }
--- appframework/src/main/java/com/alibaba/weex/commons/util/ExtensionUtils.java
+++ appframework/src/main/java/com/alibaba/weex/commons/util/ExtensionUtils.java
@@ -33,15 +33,26 @@ public class ExtensionUtils {
     }
   }
 
-  public static void registerComponents(HashMap<String, String> components) {
-    for (Map.Entry<String, String> component : components.entrySet()) {
-      registerComponent(component.getKey(), component.getValue());
+//  public static void registerComponents(HashMap<String, String> components) {
+//    for (Map.Entry<String, String> component : components.entrySet()) {
+//      registerComponent(component.getKey(), component.getValue());
+//    }
+//  }
+//
+//  public static void registerModules(HashMap<String, String> modules) {
+//    for (Map.Entry<String, String> module : modules.entrySet()) {
+//      registerModule(module.getKey(), module.getValue());
+//    }
+//  }
+  public static void registerComponents(HashMap<String, PluginEntry> components) {
+    for (Map.Entry<String, PluginEntry> component : components.entrySet()) {
+      registerComponent(component.getKey(), component.getValue().pluginClass);
     }
   }
 
-  public static void registerModules(HashMap<String, String> modules) {
-    for (Map.Entry<String, String> module : modules.entrySet()) {
-      registerModule(module.getKey(), module.getValue());
+  public static void registerModules(HashMap<String, PluginEntry> modules) {
+    for (Map.Entry<String, PluginEntry> module : modules.entrySet()) {
+      registerModule(module.getKey(), module.getValue().pluginClass);
     }
   }
 }
